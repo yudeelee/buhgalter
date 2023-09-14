2a126a4f063bf3b1b638370b0a30d82b41375873
Yura Dmytruk <study.on.work@gmail.com>
2023-09-14 14:12:48 +0300

    beginning

---
+++ app/components/sections/header/userMenu/UserMenu.jsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss';
+import Link from 'next/link';
+import React from 'react';
+
+const UserMenu = () => {
+  return (
+    <>
+      <div className={styles.userImg}></div>
+      <Link href='/login' className={styles.link}>
+        Увійти
+      </Link>
+    </>
+  );
+};
+
+export default UserMenu;
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Header from './components/sections/header/Header';
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <Header />
+    </div>
+  );
+}
